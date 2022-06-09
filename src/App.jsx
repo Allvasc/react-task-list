05b55309ce4f7f4a1d53b480fa94beae1342f6f4
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-06-09 19:36:59 -0300

    função para adicionar elemento criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import { useState } from 'react'
 import { GiHornedHelm } from 'react-icons/gi'
+import { AiOutlinePlus, AiOutLineCLose } from 'react-icons/ai'
 
 function App() {
+
+    const [tasks, setTasks] = useState([''])
+    const [input, setInput] = useState('')
+
+    const handleSubmit = (e) =>{
+      e.preventDefault()
+      const addTask = {
+        id: Math.floor(Math.random() * 1000),
+        text: input,
+        completed: false
+      }
+      setTasks([...tasks, addTask]) 
+      setInput('')
+    }
+
     return (
       <div>
         <div>
-          <h1><GiHornedHelm></GiHornedHelm>PowerList</h1>
+          <h1><GiHornedHelm/>PowerList</h1>
+
+          <form onClick={handleSubmit}>
+            <AiOutlinePlus/>
+            <input value={input} type="text" placeholder='Enter a task' onChange={e => setInput(e.target.value)}/>
+          </form>
+
+          {
+            tasks.map(task => (
+              <div key={task.id}>
+                <p>{task.text}</p>
+              </div>
+            ))}
         </div>
       </div>
     );
