47226d8135769caef789fea00e264be4a87bf53b
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-06-09 16:03:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { GiHornedHelm } from 'react-icons/gi'
+
+function App() {
+    return (
+      <div>
+        <div>
+          <h1><GiHornedHelm></GiHornedHelm>PowerList</h1>
+        </div>
+      </div>
+    );
+  }
+
+export default App;
